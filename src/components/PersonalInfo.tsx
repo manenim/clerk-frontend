593b5612248b875eea2ea12d2f1b9851e3d431bd
uccy553 <umonday98@gmail.com>
2023-02-06 17:22:09 +0100

    refactor

--- src/components/PersonalInfo.tsx
+++ src/components/PersonalInfo.tsx
@@ -12,7 +12,7 @@ const PersonalInfo = () => {
 
   return (
     <Box>
-        <Box sx={{ position: 'relative', display: 'flex', gap: { lg: '20em', xs: "2.9em" }, bottom: { lg: '2em', md: '6em', xs: '6em' }, left: '2em' }}>
+        <Box sx={{ position: 'relative', display: 'flex', gap: { lg: '20em', xs: "2.9em" }, bottom: { lg: '2em', md: '6em', xs: '6em' }, left: { lg: '2em', xl: '5em' } }}>
 
         <Box>
             <Typography onClick={handleBackButton} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', cursor: 'pointer' }}><ArrowBackIosIcon />back</Typography>
@@ -22,7 +22,8 @@ const PersonalInfo = () => {
             </Box>
         </Box>
 
-        <Box sx={{ position: 'relative', bottom: { lg: '.7em', xs: '4em' }, right: '.6em', marginLeft: '4.4em', display: 'flex', gap: '1em' }}>
+
+        <Box sx={{ position: 'relative', bottom: { lg: '.7em', xs: '4em' }, right: '.6em', marginLeft: {lg: '4.4em', xl: '8em'}, display: 'flex', gap: '1em' }}>
             <Box sx={{ display: 'flex', gap: '.6em', alignItems: 'center' }}>
                 <Box sx={{ width: '1em', height: '1em', backgroundColor: '#2F327D', textAlign: 'center', color: '#fff', borderRadius: '2px' }}>1</Box>
                 <Typography>School Profile</Typography>
@@ -42,8 +43,7 @@ const PersonalInfo = () => {
 
             
         </Box>
-
-        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', boxShadow: '0px 0px 2px rgba(0, 0, 0, 0.25)', border: '1px solid #F2F2F2', borderRadius: '10px', width: { lg: '70%', xs: '90%' }, justifyContent: 'center', margin: { lg: '0 auto' }, ml: { xs: '2.4em' }, mb: { xs: '2em' } }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', boxShadow: '0px 0px 2px rgba(0, 0, 0, 0.25)', border: '1px solid #F2F2F2', borderRadius: '10px', width: { lg: '70%', xl: '90%', xs: '90%' }, justifyContent: 'center', margin: { lg: '0 auto' }, ml: { xs: '2.4em' }, mb: { xs: '2em' } }}>
             <Box sx={{ display: 'flex', flexDirection: 'column', gap: '.9em', margin: '.9em' }}>
                 <Image src={logo} alt="logo"  />
 
@@ -55,22 +55,22 @@ const PersonalInfo = () => {
             <FormControl variant="filled" sx={{ marginBottom: '1em', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '1.5em' }}>
                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '.7em' }}>
                     <label>Full Name</label>
-                    <TextField id="filled-basic" placeholder='Enter your full name' sx={{ width: '23em' }} variant="outlined" required />
+                    <TextField id="filled-basic" placeholder='Enter your full name' sx={{ width: { lg: '23em', xl: '30em' } }} variant="outlined" required />
                </Box> 
                
                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '.7em' }}>
                     <label>Phone Number</label>
-                    <TextField id="filled-basic" placeholder='Enter school phone number' sx={{ width: '23em' }} variant="outlined" required />
+                    <TextField id="filled-basic" placeholder='Enter school phone number' sx={{ width: { lg: '23em', xl: '30em' } }} variant="outlined" required />
                </Box>
 
                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '.7em' }}>
                     <label>Email Address</label>
-                    <TextField id="filled-basic" placeholder='Enter school email address' sx={{ width: '23em' }} variant="outlined" required />
+                    <TextField id="filled-basic" placeholder='Enter school email address' sx={{ width: { lg: '23em', xl: '30em' } }} variant="outlined" required />
                </Box> 
 
                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '.7em' }}>
                     <label>Address</label>
-                    <TextField id="filled-basic" placeholder='Enter your address' sx={{ width: '23em' }} variant="outlined" required />
+                    <TextField id="filled-basic" placeholder='Enter your address' sx={{ width: { lg: '23em', xl: '30em' } }} variant="outlined" required />
                </Box> 
 
                <Button href='/setPassword' variant="contained" sx={{ width: '23em', background: '#2F327D', height: '4em' }}>
