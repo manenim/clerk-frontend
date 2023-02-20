0006286101c72384b969338e71c17d21a81ea642
Uche Monday <umonday90@proton.me>
2023-02-20 18:13:07 +0000

    Update pricing.tsx

--- src/constants/pricing.tsx
+++ src/constants/pricing.tsx
@@ -3,7 +3,7 @@ import DoneIcon from '@mui/icons-material/Done';
 export const price = [
     {
         type: "Bronze",
-        icon: <DoneIcon fontSize='1em' />,
+        icon:  <DoneIcon fontSize='small' />,
         text1: 'Add up to 20 teachers',
         text2: '20 notes and 20 summaries',
         text3: '1,100 words per note',
@@ -15,7 +15,7 @@ export const price = [
     },
     {
         type: "Silver",
-        icon: <DoneIcon fontSize='1em' />,
+        icon: <DoneIcon fontSize='small' />,
         text1: 'Add up to 40 teachers',
         text2: '20 notes and 20 summaries',
         text3: '2,200 words per note',
@@ -27,7 +27,7 @@ export const price = [
     },
     {
         type: "Gold",
-        icon: <DoneIcon fontSize='1em' />,
+        icon: <DoneIcon fontSize='small' />,
         text1: 'Add up to 80 teachers',
         text2: '80 notes and 80 summaries',
         text3: '4,400 words per note',
@@ -39,7 +39,7 @@ export const price = [
     },
     {
         type: "Diamond",
-        icon: <DoneIcon fontSize='1em' />,
+        icon: <DoneIcon fontSize='small' />,
         text1: 'Add up to 160 teachers',
         text2: '160 notes and 160 summaries',
         text3: '8,800 words per note',
