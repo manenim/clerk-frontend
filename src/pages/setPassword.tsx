3bdd3bb69bd845c7072eca2e377ab7b72455e3a1
Uche Monday <umonday90@proton.me>
2023-02-21 10:28:21 +0000

    Update setPassword.tsx

--- src/pages/setPassword.tsx
+++ src/pages/setPassword.tsx
@@ -5,8 +5,8 @@ import { Slider, SetPassword } from "@/components";
 export default function setPassword() {
   return (
   
-      <Box sx={{ display: 'grid', marginBottom: { xs: '2em' }, marginLeft: { xs: '.4em' }, gridTemplateColumns: { lg: '1fr 1fr', md: 'gridTemplateRows', xs: 'gridTemplateRows'} }}>
-        <Box >
+      <Box sx={{ display: 'grid', marginBottom: { md: '3em' }, gridTemplateColumns: { lg: '1fr 1fr', md: 'gridTemplateRows', xs: 'gridTemplateRows'} }}>
+        <Box className="slider">
           <Slider />
         </Box>
        
