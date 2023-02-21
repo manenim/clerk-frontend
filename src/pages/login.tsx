bd0218dde1ab23f8acd7167217381fd2ecd2fbb1
Uche Monday <umonday90@proton.me>
2023-02-21 10:31:50 +0000

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,8 +5,8 @@ import { Slider, Login } from "@/components";
 export default function PeronalInfo() {
   return (
   
-      <Box sx={{ display: 'grid', marginBottom: { md: '3em', xs: '2em' }, gridTemplateColumns: { lg: '1fr 1fr', md: 'gridTemplateRows', xs: 'gridTemplateRows'} }}>
-        <Box >
+      <Box sx={{ display: 'grid', marginBottom: { md: '3em' }, gridTemplateColumns: { lg: '1fr 1fr', md: 'gridTemplateRows', xs: 'gridTemplateRows'} }}>
+        <Box className="slider">
           <Slider />
         </Box>
        
